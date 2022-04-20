80c6b5151cbf75403720b794543a58b17583bb72
mlhysrszn <mlhysrszn@gmail.com>
2022-04-21 00:14:02 +0300

    base fragment added

---
+++ app/src/main/java/com/mlhysrszn/ibuy/BaseFragment.kt
@@ -0,0 +1,37 @@
+package com.mlhysrszn.ibuy
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<T : ViewDataBinding> : Fragment() {
+    private var _binding: T? = null
+    private val binding get() = _binding!!
+
+    abstract fun layoutId(): Int
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutId(), container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+        initUI()
+    }
+
+    abstract fun initUI()
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
--- app/src/main/java/com/mlhysrszn/ibuy/FavoritesFragment.kt
+++ app/src/main/java/com/mlhysrszn/ibuy/FavoritesFragment.kt
@@ -1,26 +1,11 @@
 package com.mlhysrszn.ibuy
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.mlhysrszn.ibuy.databinding.FragmentFavoritesBinding
 
-class FavoritesFragment : Fragment() {
-    private var _binding: FragmentFavoritesBinding? = null
-    private val binding get() = _binding!!
+class FavoritesFragment : BaseFragment<FragmentFavoritesBinding>() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+    override fun layoutId(): Int = R.layout.fragment_favorites
 
-    override fun onDestroy() {
-        super.onDestroy()
-        _binding = null
+    override fun initUI() {
     }
 }
