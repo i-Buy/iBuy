5e471b3276412e340274be138695d4cc47413db6
mlhysrszn <mlhysrszn@gmail.com>
2022-04-24 20:01:50 +0300

    profile fragment added

---
+++ app/src/main/java/com/mlhysrszn/ibuy/ui/ProfileFragment.kt
@@ -0,0 +1,12 @@
+package com.mlhysrszn.ibuy.ui
+
+import com.mlhysrszn.ibuy.R
+import com.mlhysrszn.ibuy.base.BaseFragment
+import com.mlhysrszn.ibuy.databinding.FragmentProfileBinding
+
+class ProfileFragment : BaseFragment<FragmentProfileBinding>() {
+    override fun layoutId(): Int = R.layout.fragment_profile
+
+    override fun initUI() {
+    }
+}
